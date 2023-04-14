4665a9770a02b167e2a2f603c20365c32d1bbb62
ThomasLonjon <lonjon.thomas@gmail.com>
2023-04-14 12:22:35 +0200

    first commit

--- frontend/src/components/ButtonGithub.jsx
+++ frontend/src/components/ButtonGithub.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PropTypes from "prop-types";
 import pictoGitHub from "../assets/img/Picto_Github.png";
 
---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import PropTypes from "prop-types";
+
+function Logo() {
+  return <img src="" alt="logoGuessWhat" />;
+}
+
+export default Logo;
