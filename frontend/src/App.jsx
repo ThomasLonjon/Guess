fdd9825acbeed0cd9fe3b032a731b56cc285b73b
Adèle Manga <adelemanga75@gmail.com>
2023-04-18 16:56:39 +0200

    i'm addiing my part of react js projet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
 
 import { useState } from 'react';
 import Timer from "./components/Timer";
+import Text from "./components/Text";
+import './App.css';
 
 function App() {
+  
   return (
+
     <div className="App">
-      <Timer time={30}/>
+      {/* <Timer time={30}/> */}
+      <br />
+      <Text student="We are group of students from the Wild Code School" guess="GUESS WHAT?" quizz="THE GREAT QUIZZ" account="CREATE ACCOUNT"  username ="Enter Pseudo: " 
+       avatar="choose avatar"  rules="CHOOSE YOUR RULES!" questions="Number of questions" time="Time per question" theme="CHOOSE YOUR THEME!"  />
+      <br />
+
+
+      
     </div>
+     )
+
      
-  );
-}
+    
+    }
+
 
 export default App;
