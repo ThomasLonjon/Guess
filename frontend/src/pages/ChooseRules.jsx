79995d952acb20a2dfd4603c9550e7cc93284828
ThomasLonjon <lonjon.thomas@gmail.com>
2023-04-24 20:49:54 +0200

    added right and wrong answers

--- frontend/src/pages/ChooseRules.jsx
+++ frontend/src/pages/ChooseRules.jsx
@@ -3,7 +3,9 @@ import NavButton from "../components/NavButton";
 import MyMap from "../components/MyMap";
 
 function ChooseRules() {
-  const [randomCountry, setRandomCountry] = useState(false);
+  const [randomCountries, setRandomCountries] = useState(null);
+  const [rightAnswer, setRightAnswer] = useState(null);
+  const [wrongAnswers, setWrongAnswers] = useState(null);
 
   //  ---------------------------------- QUESTION CAPITALS ----------------------------------
 
@@ -11,28 +13,50 @@ function ChooseRules() {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => {
-        const randomFilteredCountry = data.filter(
+        const FilteredCountry = data.filter(
           (country) =>
             country.population > 400000 && country.capitalInfo !== null
         );
-        const randomIndex = Math.floor(
-          Math.random() * randomFilteredCountry.length
-        );
-        setRandomCountry(randomFilteredCountry[randomIndex]);
+
+        // generate an array of random country out of FilteredCountry
+        const randomCountryIndex = () =>
+          Math.floor(Math.random() * FilteredCountry.length);
+
+        const countrySet = new Set();
+        while (countrySet.size < 4) {
+          countrySet.add(FilteredCountry[randomCountryIndex()]);
+        }
+        const countryArray = Array.from(countrySet);
+        setRandomCountries(countryArray);
+
+        // generate a random index to determinate right and wrong answers
+        const randomAnswerIndex = () => Math.floor(Math.random() * 4) - 1;
+        const rightAnswerArray = [countryArray.splice(randomAnswerIndex, 1)];
+        const wrongAnswersArray = [...countryArray];
+        setRightAnswer(rightAnswerArray[0]);
+        setWrongAnswers(wrongAnswersArray);
       })
+
       .catch((err) => console.error("err -->", err));
   }, []);
+
+  if (randomCountries) {
+    console.info(rightAnswer[0].capital);
+    console.info(wrongAnswers[0].capital);
+    console.info(wrongAnswers[1].capital);
+    console.info(wrongAnswers[2].capital);
+  }
 
   //  ---------------------------------- RETURN ----------------------------------
   return (
     <div className="pageStyle">
       <div>ChooseRules</div>
       <NavButton pageName="/ChooseThemes" />
-      {randomCountry ? (
+      {randomCountries ? (
         <MyMap
-          longitude={randomCountry?.capitalInfo.latlng[1]}
-          latitude={randomCountry?.capitalInfo.latlng[0]}
-          countryCode={randomCountry?.cca3}
+          longitude={rightAnswer[0]?.capitalInfo.latlng[1]}
+          latitude={rightAnswer[0]?.capitalInfo.latlng[0]}
+          countryCode={rightAnswer[0]?.cca3}
         />
       ) : (
         ""
