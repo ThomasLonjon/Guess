96d28547488b3cc21b828b960fcb138c22f3ddba
Adèle Manga <adelemanga75@gmail.com>
2023-05-03 10:07:03 +0200

    i am adding the Drinks files

--- frontend/src/components/Drinks.jsx
+++ frontend/src/components/Drinks.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 function Drinks() {
-  const [cocktail, setCocktail] = useState({});
   const [cocktails, setCocktails] = useState([]);
-  const [options, setOptions] = useState([]);
+  const [rightAnswer, setRightAnwser] = useState({});
+  const [answers, setAnswers] = useState([]);
+  const [responseObject, setResponseObject] = useState({});
   useEffect(() => {
     fetch(
       "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink"
@@ -12,58 +13,59 @@ function Drinks() {
       .then((data) => setCocktails(data.drinks))
       .catch((error) => console.info(error));
   }, []);
-  useEffect(() => {
-    const randomCocktail =
-      cocktails[Math.floor(Math.random() * cocktails.length)];
-    setCocktail(randomCocktail);
-  }, [cocktails]);
-  useEffect(() => {
-    const allCocktails = [...cocktails];
-    allCocktails.splice(allCocktails.indexOf(cocktail), 1);
-    const otherCocktails = [];
-    while (otherCocktails.length < 3) {
-      const randomCocktail =
-        allCocktails[Math.floor(Math.random() * allCocktails.length)];
-      otherCocktails.push(randomCocktail?.strDrink);
-      allCocktails.splice(allCocktails.indexOf(randomCocktail), 1);
-    }
-    const correctOption = cocktail?.strDrink;
-    setOptions(
-      [...otherCocktails, correctOption].sort(() => 0.5 - Math.random())
-    );
-  }, [cocktail, cocktails]);
-  useEffect(() => {
-    console.info("cocktail array ", options);
-  }, [options]);
-  const [answerMessage, setAnswerMessage] = useState(null);
-  const handleOptionClick = (option) => {
-    console.info(option);
-    console.info(cocktail?.strDrink);
-    if (option === cocktail.strDrink) {
-      console.info({ options, index: options.indexOf(options) });
-      setAnswerMessage("Correct answer!");
-    } else {
-      setAnswerMessage("Wrong answer!");
-    }
-  };
-  // const rightIndex = options.indexOf(cocktail);
-  // console.info("rightIndex", rightIndex);
+
   // useEffect(() => {
-  //   console.info("render");
-  // }, [answerMessage]);
+  //   // On prend une element aléatoire de cocktails pour avoir un objet cocktail qui sera la réponse
+  //   if (cocktails.length)
+  //     setRightAnwser(cocktails[Math.floor(Math.random() * cocktails.length)]);
+  // }, [cocktails.length]);
+
+  useEffect(() => {
+    if (cocktails) {
+      console.info("render");
+      const randomIndex = Math.floor(Math.random() * cocktails.length);
+
+      if (answers.length < 4) {
+        setAnswers((prevState) =>
+          [...prevState, cocktails[randomIndex]].filter(Boolean)
+        );
+      }
+      if (answers.length === 4) {
+        const randomIndex = Math.floor(Math.random() * answers.length);
+
+        setRightAnwser(answers[randomIndex]);
+      }
+    }
+  }, [answers.length, cocktails.length]);
+
+  useEffect(() => {
+    setResponseObject({
+      answers,
+      rightIndex: answers.indexOf(rightAnswer),
+    });
+  }, [answers, rightAnswer]);
+
+  useEffect(() => {
+    console.info(responseObject);
+  }, [responseObject]);
+
+  const handleOptionClick = (baguette) => {
+    // console.info({ answers, rightIndex: answers.indexOf(rightAnswer) });
+  };
+
   return (
     <div>
-      <img src={cocktail?.strDrinkThumb} alt={cocktail?.strDrink} />
-      {options.map((option) => (
+      <img src={rightAnswer?.strDrinkThumb} alt={rightAnswer?.strDrink} />
+      {answers.map((option) => (
         <button
           type="button"
           key={option}
-          onClick={() => handleOptionClick(option)}
+          onClick={() => handleOptionClick(option.strDrink)}
         >
-          {option}
+          {option.strDrink}
         </button>
       ))}
-      {answerMessage && <p>{answerMessage}</p>}
+      {/* {answerMessage && <p>{answerMessage}</p>} */}
     </div>
   );
 }
