0157a69fad53eb297813c50924d9b36793ca71a7
Adèle Manga <adelemanga75@gmail.com>
2023-05-03 17:24:42 +0200

    i'am adding AboutUs files

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,13 +1,35 @@
 import React from "react";
-import NavButton from "../components/NavButton";
 import Logo from "../components/Logo";
+import ButtonGithub from "../components/ButtonGithub";
+import Button from "../components/Button";
 
 export default function AboutUs() {
   return (
-    <div className="pageStyle">
-      <div className="about">About us</div>
-      <Logo name="logo" />
-      <NavButton pageName="/" />
-    </div>
+    <section className="pageStyle">
+      <br />
+      <br />
+      <div className="about">About Us</div>
+
+      <br />
+
+      <Logo />
+
+      {/* <div className="logo"></div> */}
+      <br />
+      <br />
+
+      <div className="buttonstudents">
+        <Button text="We are the group of students of Wild Code School" />
+      </div>
+      <br />
+      <br />
+
+      <div className="buttonsgithub">
+        <ButtonGithub devName="Adèle" devLink="./" />
+        <ButtonGithub devName="J-M" devLink="./" />
+        <ButtonGithub devName="Thomas" devLink="./" />
+        <ButtonGithub devName="Alexandre" devLink="./" />
+      </div>
+    </section>
   );
 }
