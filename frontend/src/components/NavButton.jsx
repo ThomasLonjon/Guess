10ba61fbbc8e810f7326da6bc7210b9b43dedd7d
Adèle Manga <adelemanga75@gmail.com>
2023-05-03 12:18:14 +0200

    i submit h1 and img in html

--- frontend/src/components/NavButton.jsx
+++ frontend/src/components/NavButton.jsx
@@ -4,14 +4,52 @@ import PropTypes from "prop-types";
 
 function NavButton({ pageName, onClick }) {
   return (
-    <div className="navButton">
-      <Link to={pageName}>
-        <button type="button" onClick={onClick}>
-          Navbutton
-        </button>
-        {/* <div>Navbutton </div> */}
-      </Link>
-    </div>
+    <>
+      <div className="navButton1">
+        <Link to={pageName}>
+          <button type="button" onClick={onClick}>
+            We are group of students from the Wild Code School
+          </button>
+          {/* <div>Navbutton </div> */}
+        </Link>
+      </div>
+
+      <div className="navButton">
+        <Link to={pageName}>
+          <button type="button" onClick={onClick}>
+            Adèle
+          </button>
+          {/* <div>Navbutton </div> */}
+        </Link>
+      </div>
+
+      <div className="navButton">
+        <Link to={pageName}>
+          <button type="button" onClick={onClick}>
+            Jean-Maxime
+          </button>
+          {/* <div>Navbutton </div> */}
+        </Link>
+      </div>
+
+      <div className="navButton">
+        <Link to={pageName}>
+          <button type="button" onClick={onClick}>
+            Thomas
+          </button>
+          {/* <div>Navbutton </div> */}
+        </Link>
+      </div>
+
+      <div className="navButton">
+        <Link to={pageName}>
+          <button type="button" onClick={onClick}>
+            Alexendre
+          </button>
+          {/* <div>Navbutton </div> */}
+        </Link>
+      </div>
+    </>
   );
 }
 
--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavButton from "../components/NavButton";
+import Logo from "../components/Logo";
 
 export default function AboutUs() {
   return (
     <div className="pageStyle">
       <div>About us</div>
+      <Logo />
       <NavButton pageName="/" />
     </div>
   );
